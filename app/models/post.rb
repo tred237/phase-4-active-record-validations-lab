7dafb07e2f8e55a4902dd35b8a029929b4da76b8
trevor dewindt <trevor.dewindt@gmail.com>
2023-03-09 20:58:21 -0700

    commit tree 53a298b205e6e3da9d48fba3006b4159021dd0a4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ['Fiction','Non-Fiction'] }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        criteria = ["Won't Believe","Secret","Top [number]","Guess"]
+        unless criteria.find{|e| self.title.to_s.include?(e)} || 
+            errors.add(:title, "Need to have a clickbaity title")
+        end
+    end
 end
